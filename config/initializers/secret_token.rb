8853288e499a26cffa91e4aa18e2a7542c161675
sukesh salian <sukesh1989@gmail.com>
2011-03-12 11:52:51 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chukkumark::Application.config.secret_token = '88b4aa6a31714d575bc1e492f0178fe44bbfbaa9ca42da3a2e124eab1405ca67c0fe723d5ccdf54656cbd7ad949a3038cb759d3b268a49ced44f944c60b1a53e'
